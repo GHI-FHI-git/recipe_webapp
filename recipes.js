274153cc3709c788da0d8ce3a0f6f48af4c0a6be
GHI-FHI-git <gifi2001@gmail.com>
2026-02-27 10:58:52 +0100

    Add files via upload

---
+++ recipes.js
@@ -0,0 +1,74 @@
+const defaultRecipes = [
+    {
+        "id": 1,
+        "title": "Classic Avocado Toast",
+        "image": "https://images.unsplash.com/photo-1541519227354-08fa5d50c44d?auto=format&fit=crop&w=800&q=80",
+        "ingredients": [
+            "2 slices sourdough bread",
+            "1 ripe avocado",
+            "Cherry tomatoes",
+            "Salt, pepper, and chili flakes",
+            "Drizzle of olive oil"
+        ],
+        "steps": [
+            "Toast the sourdough slices until golden brown.",
+            "Halve the avocado, remove the pit, and mash the flesh in a bowl.",
+            "Spread the mashed avocado evenly over the toast.",
+            "Top with sliced cherry tomatoes, seasoning, and olive oil."
+        ]
+    },
+    {
+        "id": 1772182250091,
+        "title": "Kartoffel-Lauch Pfanne mit Eiern",
+        "image": "https://recipecontent.fooby.ch/27682_3-2_960-640.jpg",
+        "ingredients": [
+            "4 Personen:",
+            "1kg Kartoffeln festkochend (1cm Würfel)",
+            "400g Lauch (1.5cm Streifen)",
+            "400g Halloumi (2cm Würfel)",
+            "8 Eier",
+            "2TL Za'Atar",
+            "Salz & Pfeffer"
+        ],
+        "steps": [
+            "Kartoffeln 10min dämpfen mit Deckel.",
+            "Deckel weg, 10min goldbraun anbraten.",
+            "Lauch und Halloumi 5min mitbraten.",
+            "würzen.",
+            "4 Mulden formen, Eier in die Mulde (~6min)"
+        ]
+    },
+    {
+        "id": 1772184402611,
+        "title": "Hello",
+        "image": "https://recipecontent.fooby.ch/27682_3-2_960-640.jpg",
+        "ingredients": [
+            "test"
+        ],
+        "steps": [
+            "Test"
+        ]
+    },
+    {
+        "id": 1772185573298,
+        "title": "Bolo",
+        "image": "https://rila.de/media/17/6d/19/1718612260/pastabollo.jpg",
+        "ingredients": [
+            "Fleisch"
+        ],
+        "steps": [
+            "kochen dann essen"
+        ]
+    },
+    {
+        "id": 1772185573298,
+        "title": "Bolo",
+        "image": "https://rila.de/media/17/6d/19/1718612260/pastabollo.jpg",
+        "ingredients": [
+            "Fleisch und Teigwaren"
+        ],
+        "steps": [
+            "kochen dann essen"
+        ]
+    }
+];
